9968e3331e1b77113146aee182a8be18997c115d
Teena <teenapaul2004@gmail.com>
2026-03-17 17:07:00 +0530

    api starting

--- Backend/server.js
+++ Backend/server.js
@@ -151,7 +151,9 @@ if (email) {
    SERVER
 ============================= */
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("🚀 Panacea One Backend is Running");
+});
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
